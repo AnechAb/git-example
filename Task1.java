59b9681c65588a3422b5b201316144a18c4ea6d7
AnechAb <abubekirova@inbox.ru>
2022-04-29 23:21:04 +0300

    2 commit

---
+++ Task1.java
@@ -0,0 +1,31 @@
+public class Example1 {
+
+    public static void main(String[] args) {
+//        method(1);
+        Number val = 40;
+        method((Integer) val);
+
+    }
+
+//    static int method() {
+//        System.out.println("int");
+//        return 1;
+//    }
+
+    static void method(Object val) {
+        System.out.println("void");
+    }
+
+    static void method(Integer val) {
+        System.out.println("Val");
+    }
+
+    static void method(int val) {
+        System.out.println("Val");
+    }
+
+    static void method1() {
+
+    }
+
+}
